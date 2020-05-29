a1795f77a10a2d2c695d02755b017b91e1a97948
Billy Wu <by3362@gmail.com>
2020-05-29 12:27:30 +0800

    更改重試次數 及 禁止頁面跳轉

--- cathaybk_hw2.py
+++ cathaybk_hw2.py
@@ -8,7 +8,7 @@ from PIL import Image
 import pytesseract
 import time
 import pymongo
-from fake_useragent import UserAgent
+# from fake_useragent import UserAgent
 import random
 import ssl
 import socket 
@@ -31,7 +31,7 @@ def convert_img(img,threshold):
     return img
 
 async def main(url):
-    ua = UserAgent()
+    # ua = UserAgent()
     browser = await launch({'headless': True,             # 是否啟用 Headless 模式
                             'devtools': False,             # 否為每一個頁面自動開啟調試工具，默認是 False。如果這個參數設置為 True，那麼 headless 參數就會無效，會被強制設置為 False。
                             'args': [ 
@@ -49,8 +49,8 @@ async def main(url):
                             'dumpio': True,               # 解决浏览器多开卡死
                         })
     page = await browser.newPage()
-    # await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36')
-    await page.setUserAgent(ua.random)
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36')
+    # await page.setUserAgent(ua.random)
 	# 第二步，修改 navigator.webdriver检测
 	# 其实各种网站的检测js是不一样的，这是比较通用的。有的网站会检测运行的电脑运行系统，cpu核心数量，鼠标运行轨迹等等。
     # 反爬js
@@ -68,7 +68,7 @@ async def main(url):
     ## 關閉 選擇地區 的 彈出視窗，否則 click 下一頁是無效的
     await page.click('a.area-box-close')
     page.mouse
-    await asyncio.sleep(int(random.uniform(3, 5)))
+    await asyncio.sleep(int(random.uniform(1, 3)))
     # await page.waitFor(int(random.uniform(3, 7))*1000)
     # await page.click('a.close')
     # page.mouse
@@ -90,27 +90,30 @@ async def main(url):
         data = await page.content()
         soup = BeautifulSoup(data, "html5lib")
         links = soup.select("div#content > ul.listInfo > li.pull-left.infoContent")
-        # headers = {
+        headers = {
         # 'user-agent': ua.random
-        # }
+        'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
+        }
         info_list = []
         # Link迴圈
         for link in links:
-            headers = {
-                'user-agent': ua.random
-            }
+            # headers = {
+            #     'user-agent': ua.random
+            # }
             #### 連結
             link = "https:" + link.h3.a["href"].strip()
             # verify 新增內建 SLL認證書 及 timeout
-            # 超時重試 5 次
+            # 超時重試 3 次
             times = 0
-            while times <= 5:
+            while times < 3:
                 try:
                     requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-                    link_data = requests.get(url=link, headers=headers, verify = False, timeout = (10,15))
+                    # allow_redirects 拒絕重新導向
+                    link_data = requests.get(url=link, headers=headers, verify = False, allow_redirects=False, timeout = (10,15))
                     link_soup = BeautifulSoup(link_data.text, 'html5lib')
                     # print(link)
                     ## 抓取title 跳出index error
+                    title = ""
                     try:
                         title = link_soup.select_one("div.houseInfo > h1 > span.houseInfoTitle").text.strip()
                     except IndexError as e:
@@ -122,6 +125,7 @@ async def main(url):
                         title = ""
                         print(link_soup)
                     userInfo = link_soup.find("div", "userInfo")
+                    owner = ""
                     # 有些userinfo 會有複數 div
                     try:
                         owner = userInfo.select_one("div.infoOne > div.avatarRight > div").text.strip()
@@ -142,14 +146,15 @@ async def main(url):
 
                     # img 有 text 或 圖片
                     #### 連絡電話
+                    tel_src = ""
                     tel_num = ""
-                    tel_src = userInfo.select("span.num")
                     times2 = 0
-                    while times2 <= 5:
+                    while times2 < 3:
                         try:
-                            if tel_src[0].img:
+                            tel_src = userInfo.select("span.num")
+                            if tel_src[0].img and tel_src != "":
                                 tel_src = tel_src[0].img["src"].strip()
-                                res = requests.get(url="http:" + tel_src + ".jpg", headers=headers, verify = False, timeout = (10,15))
+                                res = requests.get(url="http:" + tel_src + ".jpg", headers=headers, verify = False, allow_redirects=False, timeout = (10,15))
                                 with open("temp.jpg", "wb") as f:
                                     f.write(res.content)
                                 img = Image.open("temp.jpg")
@@ -167,43 +172,48 @@ async def main(url):
                                 break
                         except requests.exceptions.ConnectTimeout:
                             times2 += 1
-                            print("Requests Timeout Error, link : http:", tel_src, ".jpg, 重試第", times, "次")
-                            await asyncio.sleep(int(random.uniform(10, 15)))
+                            print("Requests Timeout Error, link : http:", tel_src, ".jpg, 重試第", times2, "次")
+                            await asyncio.sleep(int(random.uniform(1, 3)))
                         except requests.exceptions.SSLError:
                             times2 += 1
-                            print("Requests SSL Error, link : http:", tel_src, ".jpg, 重試第", times, "次")
-                            await asyncio.sleep(int(random.uniform(10, 15)))
+                            print("Requests SSL Error, link : http:", tel_src, ".jpg, 重試第", times2, "次")
+                            await asyncio.sleep(int(random.uniform(1, 3)))
                         except requests.exceptions.ReadTimeout:
                             times2 += 1
-                            print("Requests Read Timeout Error, link : http:", tel_src, ".jpg, 重試第", times, "次")
-                            await asyncio.sleep(int(random.uniform(10, 15)))
+                            print("Requests Read Timeout Error, link : http:", tel_src, ".jpg, 重試第", times2, "次")
+                            await asyncio.sleep(int(random.uniform(1, 3)))
                         except requests.exceptions.ConnectionError as e:
                             times2 += 1
-                            print(e, ", link : http:", tel_src, ".jpg, 重試第", times, "次")
-                            await asyncio.sleep(int(random.uniform(50, 60)))
+                            print(e, ", link : http:", tel_src, ".jpg, 重試第", times2, "次")
+                            await asyncio.sleep(int(random.uniform(5, 8)))
                         except ssl.SSLError:
                             times2 += 1
-                            print("SSL Error, link : http:", tel_src, ".jpg, 重試第", times, "次")
-                            await asyncio.sleep(int(random.uniform(10, 15)))
+                            print("SSL Error, link : http:", tel_src, ".jpg, 重試第", times2, "次")
+                            await asyncio.sleep(int(random.uniform(1, 3)))
                         except urllib3.exceptions.ProtocolError:
                             times2 += 1
-                            print("urlib3 Protocol Error, link : http:", tel_src, ".jpg, 重試第", times, "次")
-                            await asyncio.sleep(int(random.uniform(10, 15)))
-                    if times2 == 5:
+                            print("urlib3 Protocol Error, link : http:", tel_src, ".jpg, 重試第", times2, "次")
+                            await asyncio.sleep(int(random.uniform(1, 3)))
+                        except AttributeError as e:
+                            times2 += 1
+                            print(e, ", link : ", link, ", 重試第", times2, "次")
+                            print(tel_src)
+                    if times2 == 3:
                         print("http:" , tel_src, ".jpg, 連線五次皆失敗!故跳過 !")
                         tel_num = ""
 
-
+                    price = ""
                     #### 租金
                     detailInfo = link_soup.find("div", "detailInfo")
                     price = detailInfo.select("div.price > i")[0].text
 
-                    #### 型態housetype，現況situation，坪數pings，樓層floor，格局pattern，
-                    attrs = detailInfo.select("ul.attr > li")
                     housetype = ""
                     situation = ""
                     pings = ""
+                    floor = ""
                     pattern = ""
+                    #### 型態housetype，現況situation，坪數pings，樓層floor，格局pattern，
+                    attrs = detailInfo.select("ul.attr > li")
                     for attr in attrs:
                         attr_text = attr.text.strip().replace(" ", "")
                         temp_word = ""
@@ -218,12 +228,12 @@ async def main(url):
                             floor = attr_text.split(":")[1].replace(u"\xa0", u"")
                         elif "格局" in attr_text:
                             pattern = attr_text.split(":")[1].replace(u"\xa0", u"")
-                    
-                    #### 入住者需求 : 性別要求genderrequire，押金deposit，最短租期
-                    ResidentNeeds = link_soup.select("ul.labelList.labelList-1 > li")
+
                     genderrequire = ""
                     deposit = ""
                     minlease = ""
+                    #### 入住者需求 : 性別要求genderrequire，押金deposit，最短租期
+                    ResidentNeeds = link_soup.select("ul.labelList.labelList-1 > li")
                     for rns in ResidentNeeds:
                         temp_word = ""
                         rns_text = rns.text.strip().replace(" ", "")
@@ -236,7 +246,8 @@ async def main(url):
                     
                     # 關閉請求，釋放內存
                     link_data.close()
-                    await asyncio.sleep(int(random.uniform(1, 3)))
+                    # await asyncio.sleep(int(random.uniform(1, 2)))
+                    await asyncio.sleep(1)
 
                     # minlease 有值即表示此link爬取成功，故跳出迴圈進行下一筆link的爬取
                     if minlease != "":
@@ -244,30 +255,30 @@ async def main(url):
                 except requests.exceptions.ConnectTimeout:
                     times += 1
                     print("Requests Timeout Error, link : ", link, "重試第", times, "次")
-                    await asyncio.sleep(int(random.uniform(10, 15)))
+                    await asyncio.sleep(int(random.uniform(1, 3)))
                 except requests.exceptions.SSLError:
                     times += 1
                     print("Requests SSL Error, link : ", link, "重試第", times, "次")
-                    await asyncio.sleep(int(random.uniform(10, 15)))
+                    await asyncio.sleep(int(random.uniform(1, 3)))
                 except requests.exceptions.ReadTimeout:
                     times += 1
                     print("Requests Read Timeout Error, link : ", link, "重試第", times, "次")
-                    await asyncio.sleep(int(random.uniform(10, 15)))
+                    await asyncio.sleep(int(random.uniform(1, 3)))
                 except requests.exceptions.ConnectionError as e:
                     times += 1
                     print(e, ", link : ", link, "重試第", times, "次")
-                    await asyncio.sleep(int(random.uniform(50, 60)))
+                    await asyncio.sleep(int(random.uniform(5, 8)))
                 except ssl.SSLError:
                     times += 1
                     print("SSL Error, link : ", link, "重試第", times, "次")
-                    await asyncio.sleep(int(random.uniform(10, 15)))
+                    await asyncio.sleep(int(random.uniform(1, 3)))
                 except urllib3.exceptions.ProtocolError:
                     times += 1
                     print("urlib3 Protocol Error, link : ", link, "重試第", times, "次")
-                    await asyncio.sleep(int(random.uniform(10, 15)))
+                    await asyncio.sleep(int(random.uniform(1, 3)))
 
 
-                if times == 5:
+                if times == 3:
                     title = "Timeout"
                     landlord = ""
                     identity = ""
@@ -304,7 +315,8 @@ async def main(url):
             
             info_list.append(info)
             counter += 1
-            await asyncio.sleep(int(random.uniform(1, 3)))
+            # await asyncio.sleep(int(random.uniform(1, 2)))
+            await asyncio.sleep(1)
 
         # print(info_list)
         #### 發現問題 : 會有跳出選擇縣市、通知，就會導致next page 失敗!!!!!!
@@ -316,8 +328,8 @@ async def main(url):
             await page.evaluate('_ => {window.scrollBy(0, window.innerHeight);}')
             await page.click('div.pageBar > a.pageNext > span')
             page.mouse
-            await asyncio.sleep(int(random.uniform(3, 5)))
-            # await page.waitFor(int(random.uniform(3, 7))*1000)
+            # await asyncio.sleep(int(random.uniform(1, 2)))
+            await asyncio.sleep(1)
             await page.evaluate('_ => {window.scrollBy(0, window.innerHeight);}')
         # await asyncio.wait([
         #     page.click('div.pageBar > a.pageNext > span'),
